14c3e3d170341a7a2cc4c3cfb10714ac5a6b35d3
GiorgioDellutri <giorgiodll93@gmail.com>
2022-11-24 18:33:16 +0100

    dadi button

--- js/script.js
+++ js/script.js
@@ -1,15 +1,29 @@
+const utente = document.getElementById ('numero-utente');
+const pc = document.getElementById('numero-pc');
+const button = document.querySelector('a.btn');
+const risultato = document.getElementById('risultato');
 
-let numeroUtente = Math.floor(Math.random()*6+1);
-let numeroPc = Math.floor(Math.random()*8+1);
+button.addEventListener ('click', function(){
+    
+    let numeroUtente = Math.floor(Math.random()*6+1);
+    let numeroPc = Math.floor(Math.random()*8+1);
+    
+    utente.innerHTML = numeroUtente;
+    pc.innerHTML = numeroPc;
+    
+    console.log(numeroUtente);
+    console.log(numeroPc);
+
+    if ( numeroUtente < numeroPc){
+        console.log('pc ha vinto');
+        risultato.innerHTML = 'Ha vinto il computer';
+    } else if ( numeroUtente > numeroPc){
+        console.log('utente ha vinto')
+        risultato.innerHTML = 'Hai vinto tu';
+    } else if(numeroUtente === numeroPc){ 
+        console.log('parità');
+        risultato.innerHTML = 'Parità';
+    }
+})
 
 
-console.log(numeroUtente);
-console.log(numeroPc);
-
-if ( numeroUtente < numeroPc){
-    console.log('pc ha vinto')
-} else if ( numeroUtente > numeroPc){
-    console.log('utente ha vinto')
-} else if(numeroUtente === numeroPc){ 
-    console.log('parità')
-}
